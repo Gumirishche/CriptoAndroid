34e82a56b98eab775a26d55792c186af3d3e8554
Gumirishche <gumer2001@mail.ru>
2022-10-23 14:43:10 +0400

    сделал 2 фрагмента

---
+++ app/src/main/java/com/example/myapplication/ui/splash/SplashFragment.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication.ui.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentMainBinding
+import com.example.myapplication.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding?=null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding= FragmentSplashBinding.inflate(layoutInflater,container, false)
+        return mBinding.root
+    }
+}
